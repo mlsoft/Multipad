e496902641a6ead0906d1263881fc8a51f1bcff1
Martin Laberge <mlsoftlaberge@gmail.com>
2017-08-09 13:15:12 -0400

    new project Multipad

---
+++ app/src/main/java/net/ddns/mlsoftlaberge/multipad/MultipadActivity.java
@@ -0,0 +1,13 @@
+package net.ddns.mlsoftlaberge.multipad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MultipadActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.multipad_activity);
+    }
+}
